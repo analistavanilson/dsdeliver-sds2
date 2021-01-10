b835069f77eca515dc44b72915c7e095cb019fb6
unknown <analista.vanilson@gmail.com>
2021-01-09 23:54:25 -0500

    Add Home page

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,27 @@
+import './styles.css';
+import { ReactComponent as MainImage} from './main.svg';
+
+function Home() {
+    return (
+        <div className="home-continer">
+            <div className="home-content">
+                <div className="home-actions">
+                    <h1 className="home-title">
+                        Faça seu pedido <br/> que entregamos <br/> para voce!!!
+                    </h1>
+                    <h3 className="home-subtitle">
+                        Escolha o seu pedido e em poucos minutos <br/> levaremos na sua porta
+                    </h3>
+                    <a href="orders" className="home-btn-order">
+                        FAER PEDIDO
+                    </a>
+                </div>
+                <div className="home-image">
+                    <MainImage />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
